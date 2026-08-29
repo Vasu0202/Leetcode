818f612ecd2d441b4decf7c3fd6aa42742071a77
Vasu0202 <127432191+Vasu0202@users.noreply.github.com>
2026-08-29 17:44:07 +0530

    Time: 3 ms (64.49%) | Memory: 8.3 MB (11.43%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    unordered_map<int,int> dp;
+
+    int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+
+        //dp find
+        if(dp.find(n)!=dp.end()){
+            return dp[n];
+        }
+
+        int ans=fib(n-1)+fib(n-2);
+        dp[n]=ans;
+        return ans;
+    }
+};
